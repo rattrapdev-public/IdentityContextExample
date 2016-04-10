17683cadda45847df59db96cbc5a444273bc5637
mwinger <mateo6379@gmail.com>
2016-04-10 10:23:50 -0500

    fixup! App UI

--- IdentificationAndAuthorizationContext/IdentityWeb/Modules/Applications/ApplicationAdminModule.cs
+++ IdentificationAndAuthorizationContext/IdentityWeb/Modules/Applications/ApplicationAdminModule.cs
@@ -2,6 +2,9 @@
 using RattrapDev.Identity.Application;
 using Nancy.ModelBinding;
 using Nancy;
+using Nancy.Validation;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IdentityWeb
 {
@@ -19,6 +22,21 @@ namespace IdentityWeb
 			Post ["/"] = parameters => 
 			{
 				var viewModel = this.Bind<AppViewModel>();
+
+				var validationResult = this.Validate(viewModel);
+				var errorMessages = new List<string>();
+				foreach (var errorResult in validationResult.Errors.Values) {
+					errorMessages.AddRange(errorResult.Select(e => e.ErrorMessage));
+				}
+
+				if (!validationResult.IsValid) 
+				{
+					ViewBag.ErrorMessages = string.Join("|", errorMessages);
+					return View["Views/Admin/AppAdminDetail", viewModel];
+				}
+
+				ViewBag.ErrorMessages = string.Join("|", errorMessages);
+
 				viewModel = appService.SaveApp(viewModel);
 
 				return Response.AsRedirect ("~/admin/apps/" + viewModel.Id + "?result=" + AppResult.SaveExistingApp);
