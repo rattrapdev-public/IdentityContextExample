6dff8c2dd51b22022887a157e18b8795e0378292
mwinger <mateo6379@gmail.com>
2016-04-11 12:34:47 -0500

    Contact Email Validation

--- IdentificationAndAuthorizationContext/IdentityWeb/Modules/Client/AddClientValidator.cs
+++ IdentificationAndAuthorizationContext/IdentityWeb/Modules/Client/AddClientValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FluentValidation;
 using RattrapDev.Identity;
+using System.Net.Mail;
 
 namespace IdentityWeb
 {
@@ -11,6 +12,21 @@ namespace IdentityWeb
 			RuleFor (client => client.ClientName).NotEmpty ().WithMessage ("The client name is required!");
 			RuleFor (client => client.ContactName).NotEmpty ().WithMessage ("The contact name is required!");
 			RuleFor (client => client.ContactPhone).NotEmpty ().WithMessage ("The contact phone is required!");
+			RuleFor (client => client.ContactEmail).NotEmpty ().WithMessage ("The contact email is required!");
+			RuleFor (client => client.ContactEmail).Must ((email) => 
+				{
+					if (string.IsNullOrWhiteSpace(email)) 
+						return true;
+					try 
+					{
+						new MailAddress(email);
+						return true;
+					}
+					catch (Exception) 
+					{
+						return false;
+					}
+				}).WithMessage ("The contact email must be a valid email!");
 		}
 	}
 }
