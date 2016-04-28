bbac45366504496e279442eb088d2ba53463004d
mwinger <mateo6379@gmail.com>
2016-04-27 21:09:37 -0500

    LoginInfo implementation
    Remove username type

---
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity.Tests/Domain/Users/LoginInfoTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using RattrapDev.Identity.Domain.Users;
+using Shouldly;
+
+namespace RattrapDev.Identity.Tests
+{
+	[TestFixture]
+	public class LoginInfoTests
+	{
+		[Test]
+		public void Constructor_sets_values() 
+		{
+			var loginInfo = new LoginInfo ("username", "password");
+			loginInfo.Username.ShouldBe ("username");
+			loginInfo.ValidatePassword("password").ShouldBeTrue();
+		}
+
+		[TestCase("")]
+		[TestCase(null)]
+		[TestCase(" ")]
+		public void Constructor_invalid_username_throws_exception(string invalidUsername) 
+		{
+			Should.Throw<ArgumentException> (() => new LoginInfo(invalidUsername, "password"));
+		}
+
+		[TestCase("")]
+		[TestCase(null)]
+		[TestCase(" ")]
+		public void Constructor_invalid_password_throws_exception(string invalidPassword) 
+		{
+			Should.Throw<ArgumentException> (() => new LoginInfo("username", invalidPassword));
+		}
+
+		[Test]
+		public void Equals_returns_true_for_same_reference() 
+		{
+			var loginInfo = new LoginInfo ("username", "password");
+			loginInfo.Equals (loginInfo).ShouldBeTrue ();
+		}
+
+		[Test]
+		public void Equals_returns_false_for_null_other_value() 
+		{
+			var loginInfo = new LoginInfo ("username", "password");
+			loginInfo.Equals (null).ShouldBeFalse ();
+		}
+
+		[Test]
+		public void Equals_different_references_are_still_equal() 
+		{
+			var loginInfo1 = new LoginInfo ("username", "password");
+			var loginInfo2 = new LoginInfo ("username", "password");
+			loginInfo1.Equals (loginInfo2).ShouldBeTrue ();
+		}
+
+		[Test]
+		public void Equals_returns_false_for_non_equal_values() 
+		{
+			var loginInfo1 = new LoginInfo ("username1", "password");
+			var loginInfo2 = new LoginInfo ("username", "password");
+			loginInfo1.Equals (loginInfo2).ShouldBeFalse ();
+		}
+	}
+}
+
