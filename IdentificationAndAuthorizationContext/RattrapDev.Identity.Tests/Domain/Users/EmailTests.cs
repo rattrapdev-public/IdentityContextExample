4793cc60fd2bb4fd44afdc2bc6e6507c5ea45a43
mwinger <mateo6379@gmail.com>
2016-04-29 21:28:54 -0500

    Implement User email

---
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity.Tests/Domain/Users/EmailTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using RattrapDev.Identity.Domain.Users;
+using Shouldly;
+
+namespace RattrapDev.Identity.Tests
+{
+	[TestFixture]
+	public class EmailTests
+	{
+		[Test]
+		public void Constructor_sets_value() 
+		{
+			var email = new Email ("john@doe.com");
+			email.EmailAddress.ShouldBe ("john@doe.com");
+		}
+
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase(null)]
+		public void Constructor_invalid_email_throws_exception(string invalidEmail) 
+		{
+			Should.Throw<ArgumentException> (() => new Email (invalidEmail));	
+		}
+
+		[Test]
+		public void Constructor_malformed_email_throws_exception() 
+		{
+			Should.Throw<FormatException> (() => new Email ("john@"));	
+		}
+	}
+}
+
--- IdentificationAndAuthorizationContext/RattrapDev.Identity/Domain/Users/Email.cs
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity/Domain/Users/Email.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using RattrapDev.DDD;
+using System.Net.Mail;
 
 namespace RattrapDev.Identity.Domain.Users
 {
 	public class Email : IValueObject
 	{
-		public Email ()
+		private string emailAddress;
+
+		public Email (string emailAddress)
 		{
+			EmailAddress = emailAddress;
+		}
+
+		public string EmailAddress 
+		{
+			get 
+			{
+				return emailAddress;
+			}
+			set 
+			{
+				if (string.IsNullOrWhiteSpace (value)) 
+				{
+					throw new ArgumentException ("Email : Is Invalid!");
+				}
+
+				new MailAddress (value);
+
+				emailAddress = value;
+			}
 		}
 	}
 }
