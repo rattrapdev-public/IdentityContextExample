36e736595f21fa539c3bdab5baf943361782adb0
mwinger <mateo6379@gmail.com>
2016-04-29 21:11:59 -0500

    Implement Name

---
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity.Tests/Domain/Users/NameTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using RattrapDev.Identity.Domain.Users;
+using Shouldly;
+
+namespace RattrapDev.Identity.Tests
+{
+	[TestFixture]
+	public class NameTests
+	{
+		public void Constructor_sets_values() 
+		{
+			var name = new Name ("John", "Doe");
+			name.FirstName.ShouldBe ("John");
+			name.LastName.ShouldBe ("Doe");
+		}
+
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase(null)]
+		public void Constructor_invalid_firstName_throws_exception(string invalidFirstName) 
+		{
+			Should.Throw<ArgumentException> (() => new Name (invalidFirstName, "Doe"));	
+		}
+
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase(null)]
+		public void Constructor_invalid_lastName_throws_exception(string invalidLastName) 
+		{
+			Should.Throw<ArgumentException> (() => new Name ("John", invalidLastName));	
+		}
+
+		[Test]
+		public void Equals_returns_true_for_same_reference() 
+		{
+			var name = new Name ("John", "Doe");
+			name.Equals (name).ShouldBeTrue ();
+		}
+
+		[Test]
+		public void Equals_returns_false_for_null_other_value() 
+		{
+			var name = new Name ("John", "Doe");
+			name.Equals (null).ShouldBeFalse ();
+		}
+
+		[Test]
+		public void Equals_different_references_are_still_equal() 
+		{
+			var name1 = new Name ("John", "Doe");
+			var name2 = new Name ("John", "Doe");
+			name1.Equals (name2).ShouldBeTrue ();
+		}
+
+		[Test]
+		public void Equals_returns_false_for_non_equal_values() 
+		{
+			var name1 = new Name ("John", "Doe");
+			var name2 = new Name ("Johny", "Doe");
+			name1.Equals (name2).ShouldBeFalse ();
+		}
+	}
+}
+
--- IdentificationAndAuthorizationContext/RattrapDev.Identity/Domain/Users/Name.cs
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity/Domain/Users/Name.cs
@@ -3,10 +3,64 @@ using RattrapDev.DDD;
 
 namespace RattrapDev.Identity.Domain.Users
 {
-	public class Name : IValueObject
+	public class Name : IValueObject, IEquatable<Name>
 	{
-		public Name ()
+		private string firstName;
+		private string lastName;
+
+		public Name (string firstName, string lastName)
 		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+
+		public string FirstName 
+		{
+			get 
+			{
+				return firstName;
+			}
+			set 
+			{
+				if (string.IsNullOrWhiteSpace (value)) 
+				{
+					throw new ArgumentException ("First Name : Is invalid!");
+				}
+
+				firstName = value;
+			}
+		}
+
+		public string LastName 
+		{
+			get 
+			{
+				return lastName;
+			}
+			set 
+			{
+				if (string.IsNullOrWhiteSpace (value)) 
+				{
+					throw new ArgumentException ("Last Name : Is invalid!");
+				}
+
+				lastName = value;
+			}
+		}
+
+		public bool Equals (Name other)
+		{
+			if (other == null) 
+			{
+				return false;
+			}
+
+			if (ReferenceEquals (this, other)) 
+			{
+				return true;
+			}
+
+			return FirstName.Equals (other.FirstName) && LastName.Equals (other.LastName);
 		}
 	}
 }
