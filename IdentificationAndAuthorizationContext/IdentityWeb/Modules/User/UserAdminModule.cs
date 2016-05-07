89dd2110a863a99e8113a3a99a7e2474e0ecae20
mwinger <mateo6379@gmail.com>
2016-05-07 18:30:25 -0500

    User UI implementation

--- IdentificationAndAuthorizationContext/IdentityWeb/Modules/User/UserAdminModule.cs
+++ IdentificationAndAuthorizationContext/IdentityWeb/Modules/User/UserAdminModule.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using Nancy;
+using RattrapDev.Identity.Application;
+using Nancy.ModelBinding;
 
 namespace IdentityWeb
 {
 	public class UserAdminModule : NancyModule
 	{
-		public UserAdminModule () : base("/admin/users")
+		public UserAdminModule (IUserService userService) : base("/admin/users")
 		{
 			Get ["/"] = parameters => 
 			{
-				return View["Views/Admin/UserAdminSearch"];
+				var userResults = userService.GetAllUsers();
+				return View["Views/Admin/UserAdminSearch", userResults];
+			};
+
+			Post ["/"] = parameters => 
+			{
+				var viewModel = this.Bind<UserViewModel>();
+
+				viewModel = userService.SaveUser(viewModel);
+
+				return Response.AsRedirect("~/admin/user/" + viewModel.UserId);
+			};
+
+			Get ["/{UserId:Guid}"] = parameters => 
+			{
+				Guid userId = (Guid)parameters.UserId;
+				var userViewModel = userService.GetUser(userId);
+
+				return View["Views/Admin/UserAdminDetail", userViewModel];
+			};
+
+			Get ["/new"] = parameters => 
+			{
+				var emptyViewModel = new UserViewModel();
+				return View["Views/Admin/UserAdminDetail", emptyViewModel];
 			};
 		}
 	}
