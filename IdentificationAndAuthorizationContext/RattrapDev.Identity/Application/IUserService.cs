746806831267449ef25ac6750797a09539979dd3
mwinger <mateo6379@gmail.com>
2016-05-06 20:21:46 -0500

    Implement User Service

---
+++ IdentificationAndAuthorizationContext/RattrapDev.Identity/Application/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RattrapDev.Identity.Application
+{
+	public interface IUserService
+	{
+		IReadOnlyList<UserSearchResult> GetAllUsers();
+		UserViewModel GetUser(Guid userId);
+		UserViewModel SaveUser(UserViewModel viewModel);
+	}
+}
+
