f70794a74634af6b94cd8450044dc6df814f4133
mwinger <matt.winger@geonetric.com>
2016-05-10 16:11:33 -0500

    Fix Nancy UI tests

--- IdentificationAndAuthorizationContext/IdentityWebTests/CustomTestBootstrapper.cs
+++ IdentificationAndAuthorizationContext/IdentityWebTests/CustomTestBootstrapper.cs
@@ -7,8 +7,6 @@ namespace IdentityWebTests
 
     public class CustomTestBootstrapper : CustomBootstrapper
 	{
-		private CustomRootPathProvider customRootPathProvider = new CustomRootPathProvider();
-
 		public CustomTestBootstrapper() : base() 
 		{
 		}
@@ -20,12 +18,6 @@ namespace IdentityWebTests
 		protected override void ApplicationStartup (Autofac.ILifetimeScope container, Nancy.Bootstrapper.IPipelines pipelines)
 		{
 			base.ApplicationStartup (container, pipelines);
-		}
-
-		protected override Nancy.IRootPathProvider RootPathProvider {
-			get {
-				return customRootPathProvider;
-			}
 		}
 
 		public IClientService ClientService { get; private set; }
