5bbb466be034b6499101836532a0f3fcf48331c4
mwinger <matt.winger@geonetric.com>
2016-05-10 16:50:10 -0500

    User fluent validation

---
+++ IdentificationAndAuthorizationContext/IdentityWeb/Modules/User/UserValidator.cs
@@ -0,0 +1,35 @@
+﻿namespace IdentityWeb.Modules.User
+{
+    using System;
+    using System.Net.Mail;
+
+    using FluentValidation;
+
+    using Geonetric.Identity.Application;
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+        public UserValidator()
+        {
+            this.RuleFor(user => user.Username).NotEmpty().WithMessage("The username is required!");
+            this.RuleFor(user => user.Password).NotEmpty().WithMessage("The password is required!");
+            this.RuleFor(user => user.ClientId).NotEqual(Guid.Empty).WithMessage("The ClientId is required!");
+            this.RuleFor(user => user.FirstName).NotEmpty().WithMessage("The first name is required!");
+            this.RuleFor(user => user.LastName).NotEmpty().WithMessage("The last name is required!");
+            this.RuleFor(user => user.Email).NotEmpty().WithMessage("The email is required!");
+            this.RuleFor(user => user.Email).Must((email) =>
+            {
+                if (string.IsNullOrWhiteSpace(email))
+                    return true;
+                try
+                {
+                    new MailAddress(email);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }).WithMessage("The contact email must be a valid email!");
+        }
+    }
+}
